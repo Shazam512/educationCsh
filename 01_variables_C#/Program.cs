6528dc7c11bc4d575d19cc16cb24fb9be3ea22e3
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-04-06 20:54:00 +0300

    Add files via upload

---
+++ 01_variables_C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string namePlayer = "Hero";
+        int health = 100;
+        int damag = 30;
+
+        System.Console.WriteLine("Имя героя: " + namePlayer);
+        System.Console.WriteLine("Здоровья игрока: " + heath);
+
+        heath -= damag;
+        System.Console.WriteLine($"Вы получили урон {damag}, у вас осталось {heath} здоровья");
+    }
+}
---
+++ 02_function_C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int health = 100;
+        string name = "Hero";
+        int mana = 50;
+
+        health = TakeDamage(name, health, 50);
+        health = TakeDamage(name, health, 70);
+        mana = CastSpell(mana, 25);
+        mana = CastSpell(mana, 50);
+    }
+    static int TakeDamage(string targetName, int currentHealth, int damage)
+    {
+        int newHealth = currentHealth - damage;
+
+        if (newHealth <= 0)
+        {
+            System.Console.WriteLine($"{targetName} погиб!");
+            return 0;
+        }
+        else
+        {
+            System.Console.WriteLine($"{targetName} получил {damage} урон. Осталось {newHealth}");
+            return newHealth;
+        }
+    }
+    static int CastSpell(int currentMana, int cost)
+    {
+        int newMana = currentMana - cost;
+        if (newMana >= 0)
+        {
+            System.Console.WriteLine("Недостаточно маны!");
+            return 0;
+        }
+        else
+        {
+            System.Console.WriteLine("Заклинание применено!");
+            return newMana;
+        }
+    }
+}
